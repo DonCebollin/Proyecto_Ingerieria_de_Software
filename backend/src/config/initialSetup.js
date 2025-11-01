faba40d86190d45d02e65b3282a05b0079547086
Cristopher <cristopher.alarcon2201@alumnos.ubiobio.cl>
2025-11-01 18:20:07 -0300

    Backend de Peticiones

--- backend/src/config/initialSetup.js
+++ backend/src/config/initialSetup.js
@@ -13,7 +13,7 @@ async function createUsers() {
     await Promise.all([
       userRepository.save(
         userRepository.create({
-          nombreCompleto: "Diego Alexis Salazar Jara",
+          nombreCompleto: "Administrador General",
           rut: "21.308.770-3",
           email: "administrador2024@gmail.cl",
           password: await encryptPassword("admin1234"),
@@ -22,57 +22,21 @@ async function createUsers() {
       ),
       userRepository.save(
         userRepository.create({
-          nombreCompleto: "Diego Sebastián Ampuero Belmar",
+          nombreCompleto: "El Evaluador",
           rut: "21.151.897-9",
           email: "usuario1.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
+          password: await encryptPassword("eval1234"),
+          rol: "docente",
         })
       ),
         userRepository.save(
           userRepository.create({
-            nombreCompleto: "Alexander Benjamín Marcelo Carrasco Fuentes",
+            nombreCompleto: "El Esudiante",
             rut: "20.630.735-8",
             email: "usuario2.2024@gmail.cl",
-            password: await encryptPassword("user1234"),
-            rol: "usuario",
+            password: await encryptPassword("estu1234"),
+            rol: "estudiante",
           }),
-      ),
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Pablo Andrés Castillo Fernández",
-          rut: "20.738.450-K",
-          email: "usuario3.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
-        }),
-      ),
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Felipe Andrés Henríquez Zapata",
-          rut: "20.976.635-3",
-          email: "usuario4.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
-        }),
-      ),
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Diego Alexis Meza Ortega",
-          rut: "21.172.447-1",
-          email: "usuario5.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
-        }),
-      ),
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Juan Pablo Rosas Martin",
-          rut: "20.738.415-1",
-          email: "usuario6.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
-        }),
       ),
     ]);
     console.log("* => Usuarios creados exitosamente");
--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -80,7 +80,7 @@ export async function registerService(user) {
       email,
       rut,
       password: await encryptPassword(user.password),
-      rol: "usuario",
+      rol: "estudiante",
     });
 
     await userRepository.save(newUser);
